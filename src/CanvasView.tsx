f077076680999d89de8be6f2357726fe90425c66
gonzadelarge <gonzadelarge@gmail.com>
2025-04-26 15:03:35 +0200

    add models

--- src/CanvasView.tsx
+++ src/CanvasView.tsx
@@ -1,37 +1,89 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useEffect } from 'react'
 
-import * as THREE from 'three'
+import * as THREE from 'three';
 
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { OBJLoader } from 'three-stdlib';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas, useLoader, useFrame } from '@react-three/fiber'
 
-function Box(props: ThreeElements['mesh']) {
-    const meshRef = useRef<THREE.Mesh>(null!)
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-    useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+function Model() {
+    const ref = useRef<THREE.Object3D>(null);
+
+    const object = useLoader(OBJLoader, '/models/ring.obj');
+
+    useFrame((_state, delta) => {
+        if (ref.current) {
+            ref.current.rotation.y += delta * 0.2; // 0.2 radians per second
+        }
+    });
+
+    useEffect(() => {
+        if (ref.current) {
+            const size = new THREE.Vector3();
+            const center = new THREE.Vector3();
+            const box = new THREE.Box3().setFromObject(ref.current);
+
+            box.getSize(size);
+            box.getCenter(center);
+
+            ref.current.position.sub(center);
+
+            const maxAxis = Math.max(size.x, size.y, size.z);
+
+            ref.current.scale.multiplyScalar(1 / maxAxis);
+        }
+    }, []);
+
     return (
-        <mesh
-            {...props}
-            ref={meshRef}
-            scale={active ? 1.5 : 1}
-            onClick={(event) => setActive(!active)}
-            onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : '#2f74c0'} />
-        </mesh>
-    )
+        <primitive ref={ref} object={object} />
+    );
+}
+
+function SphereModel() {
+    const ref = useRef<THREE.Object3D>(null);
+    const obj = useLoader(OBJLoader, '/models/sphere.obj'); // <- path to your sphere
+
+    useEffect(() => {
+        if (ref.current) {
+            const size = new THREE.Vector3();
+            const center = new THREE.Vector3();
+            const box = new THREE.Box3().setFromObject(ref.current);
+
+            box.getSize(size);
+            box.getCenter(center);
+
+            ref.current.position.sub(center);
+
+            const maxAxis = Math.max(size.x, size.y, size.z);
+            ref.current.scale.multiplyScalar(0.41 / maxAxis);
+
+            ref.current.position.y += 0.23;
+        }
+    }, []);
+
+    return (
+        <primitive ref={ref} object={obj} />
+    );
 }
 
 export default function CanvasView() {
     return (
         <div className="w-full h-full">
-            <Canvas>
-                <ambientLight intensity={Math.PI / 2} />
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-                <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-                <Box position={[-1.2, 0, 0]} />
-                <Box position={[1.2, 0, 0]} />
+            <Canvas
+                style={{ background: '#14141d' }}
+                camera={{ position: [2, 2, 2], fov: 50 }}
+            >
+
+                <ambientLight intensity={0.5} />
+                <directionalLight position={[5, 5, 5]} intensity={1} />
+                <directionalLight position={[-5, 5, 5]} intensity={0.5} />
+
+                <Model />
+                <SphereModel />
+
+                <OrbitControls
+                    minPolarAngle={Math.PI / 3}
+                    maxPolarAngle={Math.PI / 3} />
             </Canvas>
         </div>
     )
