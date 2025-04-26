991885f5949e9b2396fd8b590ee1009d36023f36
gonzadelarge <gonzadelarge@gmail.com>
2025-04-26 13:32:51 +0200

    add three js

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 import { ThemeProvider } from "./components/themeProvider"
 import Layout from './Layout'
+import CanvasView from './CanvasView'
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Layout>Testing</Layout>
+      <Layout>
+        <CanvasView />
+      </Layout>
     </ThemeProvider>
   )
 }
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,14 +3,21 @@ import React from 'react'
 import { AppSidebar } from './components/AppSidebar'
 import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar"
 
+const Trigger = () => (
+    <div style={{ position: 'absolute', top: 10, zIndex: 1 }}>
+        <SidebarTrigger />
+    </div>
+)
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <SidebarProvider>
-            <AppSidebar />
-            <main>
-                <SidebarTrigger />
-                {children}
-            </main>
+        <SidebarProvider defaultOpen>
+            <div className="flex h-screen w-full">
+                <AppSidebar />
+                <main className="flex-1 relative">
+                    {children}
+                </main>
+            </div>
         </SidebarProvider>
     )
 }
